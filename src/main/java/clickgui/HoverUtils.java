2dda132a8035819013972af17c74ce61f12be900
TempleTerry[x64] <123938029+PhilipPanda@users.noreply.github.com>
2023-10-30 09:51:54 +0100

    Delete src/main directory

--- src/main/java/clickgui/HoverUtils.java
+++
@@ -1,7 +0,0 @@
-package clickgui;
-
-public class HoverUtils {
-    public static boolean hovered(int mouseX, int mouseY, int x, int y, int width, int height) {
-        return mouseX > x && mouseY > y && mouseX < width && mouseY < height;
-    }
-}
