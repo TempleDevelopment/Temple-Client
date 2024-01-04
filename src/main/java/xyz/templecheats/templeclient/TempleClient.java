9a6b9f82fc472e5c67098a9b0b8f3ec5697da0a4
PhilipPanda <philippanda2007@gmail.com>
2024-01-05 00:04:05 +0100

    Revert "1.8.1"
    This reverts commit 60dbc34eec435457d2106dc48cd0c2fd6bbec7b9.

--- src/main/java/xyz/templecheats/templeclient/TempleClient.java
+++ src/main/java/xyz/templecheats/templeclient/TempleClient.java
@@ -1,6 +1,7 @@
 package xyz.templecheats.templeclient;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.Session;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -11,82 +12,59 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.client.event.ClientChatEvent;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.opengl.Display;
-//import xyz.templecheats.templeclient.api.config.ConfigManager;
-import xyz.templecheats.templeclient.api.config.ShutdownHook;
-import xyz.templecheats.templeclient.api.config.rewrite.ConfigManager;
-import xyz.templecheats.templeclient.impl.command.CommandManager;
-import xyz.templecheats.templeclient.impl.gui.clickgui.ClickGuiManager;
-import xyz.templecheats.templeclient.impl.gui.clickgui.setting.SettingsManager;
-import xyz.templecheats.templeclient.impl.gui.font.FontUtils;
-import xyz.templecheats.templeclient.impl.gui.menu.GuiEventsListener;
-import xyz.templecheats.templeclient.impl.gui.ui.watermark;
-import xyz.templecheats.templeclient.api.event.EventManager;
-import xyz.templecheats.templeclient.api.util.keys.key;
+import xyz.templecheats.templeclient.gui.clickgui.ClickGuiManager;
+import xyz.templecheats.templeclient.gui.clickgui.setting.SettingsManager;
+import xyz.templecheats.templeclient.gui.menu.GuiEventsListener;
+import xyz.templecheats.templeclient.gui.ui.watermark;
+import xyz.templecheats.templeclient.event.EventManager;
+import xyz.templecheats.templeclient.util.keys.key;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
 
 import java.lang.reflect.Field;
 
 @Mod(modid = TempleClient.MODID, name = TempleClient.NAME, version = TempleClient.VERSION)
 public class TempleClient {
-    public static String name = "Temple Client 1.8.1";
-
     public static final String MODID = "templeclient";
     public static final String NAME = "Temple Client";
-    public static final String VERSION = "1.8.1";
+    public static final String VERSION = "1.8.0";
 
-    public static SettingsManager settingsManager;
-    public static ModuleManager moduleManager;
+    public static TempleClient instance;
+    public SettingsManager settingsManager;
     public static EventManager clientEventManager;
-    public static CommandManager commandManager;
-    public static ClickGuiManager clickGui;
-    public static ConfigManager configManager;
+    public ClickGuiManager clickGui;
+
     private static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        Display.setTitle("Loading " + name);
+        Display.setTitle("Loading " + Client.name);
         logger = event.getModLog();
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        Display.setTitle(name);
-        MinecraftForge.EVENT_BUS.register(new TempleClient());
-
         clientEventManager = new EventManager();
         MinecraftForge.EVENT_BUS.register(clientEventManager);
 
+        clickGui = new ClickGuiManager();
+        instance = this;
         settingsManager = new SettingsManager();
 
-        (TempleClient.moduleManager = new ModuleManager()).initMods();
-        logger.info("Module Manager Loaded.");
-
-        (TempleClient.commandManager = new CommandManager()).commandInit();
-        logger.info("Commands Loaded.");
-
-        MinecraftForge.EVENT_BUS.register(clientEventManager);
-        MinecraftForge.EVENT_BUS.register(commandManager);
+        Client.startup();
 
         MinecraftForge.EVENT_BUS.register(new key());
         MinecraftForge.EVENT_BUS.register(new watermark());
         MinecraftForge.EVENT_BUS.register(new GuiEventsListener());
-
-        clickGui = new ClickGuiManager();
-
-        FontUtils.bootstrap();
-
-        configManager = new ConfigManager();
-
-        logger.info("Initialized Config!");
-
-        configManager.loadModules();
-
-        Runtime.getRuntime().addShutdownHook(new ShutdownHook());
+        MinecraftForge.EVENT_BUS.register(this);
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
     @SubscribeEvent
     public void onChat(ClientChatEvent event) {
         if (event.getMessage().startsWith(".")) {
-            if (ModuleManager.commandManager.executeCommand(event.getMessage())) {
+            if (Client.commandManager.executeCommand(event.getMessage())) {
                 event.setCanceled(true);
             }
         }
@@ -112,14 +90,10 @@ public class TempleClient {
             session.set(Minecraft.getMinecraft(), s);
             session.setAccessible(false);
 
-            name = "TempleClient 1.12.2 | User: " + Minecraft.getMinecraft().getSession().getUsername();
-            Display.setTitle(name);
+            Client.name = "TempleClient 1.12.2 | User: " + Minecraft.getMinecraft().getSession().getUsername();
+            Display.setTitle(Client.name);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public static ModuleManager getModuleManager() {
-        return TempleClient.moduleManager;
     }
 }
