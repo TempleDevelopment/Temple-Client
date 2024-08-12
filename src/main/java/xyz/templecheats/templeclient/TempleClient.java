a58ddb8dc28362d395747e5764384a8b3c0fa2df
TempleTerry[x64] <123938029+PhilipPanda@users.noreply.github.com>
2024-08-12 22:58:54 +0200

    1.9.7 :confetti_ball:

--- src/main/java/xyz/templecheats/templeclient/TempleClient.java
+++ src/main/java/xyz/templecheats/templeclient/TempleClient.java
@@ -33,7 +33,7 @@ import java.lang.reflect.Field;
 public class TempleClient {
     public static final String MODID = "templeclient";
     public static final String NAME = "Temple Client";
-    public static final String VERSION = "1.9.6";
+    public static final String VERSION = "1.9.7";
     public static final MethodHandles.Lookup LOOKUP = MethodHandles.lookup();
     public static String name = NAME + " " + VERSION;
     public static AnnotatedEventManager eventBus;
